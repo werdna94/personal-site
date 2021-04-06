40486a120548e76288c78cd78216a098408fe835
Andrew Gwilliam <andrewthemandrew32@gmail.com>
2021-04-05 18:13:04 -0600

    Update project structure, add new components for pages, add routing, add icon assets, add footer component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LandingPageComponent},
+  {path: 'blog', component: BlogComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'projects', component: ProjectsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainHeaderComponent } from './main-header/main-header.component';
+import { MainHeaderComponent } from './page-accents/main-header/main-header.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { FooterComponent } from './page-accents/footer/footer.component';
+import { MainFooterComponent } from './page-accents/main-footer/main-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainHeaderComponent,
+    BlogComponent,
+    LandingPageComponent,
+    AboutComponent,
+    ProjectsComponent,
+    FooterComponent,
+    MainFooterComponent,
   ],
   imports: [
     BrowserModule,
